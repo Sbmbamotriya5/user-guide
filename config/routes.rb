feb2d0a84026b26f6ec302575105a4c0506c4c49
Sbmbamotriya5 <shubhambamotriya143@gmail.com>
2023-03-14 13:28:23 +0530

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # get "articles" ,to: "articles#new"
+  # post "articles" ,to: "articles#create"
+  # put "articles/id" ,to: "articles#upadte"
+  # delete "articles/id" ,to: "articles#destroy"
+  
+  resources :articles
+  
 end
